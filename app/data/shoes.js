49c76b7c46439ee172af95d3306d6c229cd97de9
Tyga <b_harrell17@outlook.com>
2019-02-19 23:44:06 -0700

    photo update

--- app/data/shoes.js
+++ app/data/shoes.js
@@ -1,14 +1,14 @@
 const shoeData = [ {
 	name : "Nike Air off-white pesto",
-	photo : "https://www.google.com/search?biw=1103&bih=682&tbm=isch&sa=1&ei=PyxWXImABcKT0gLI_I2wBw&q=nike+air+presto&oq=nike+air+presto&gs_l=img.3..0l10.10982.13498..13771...0.0..0.163.1459.11j4......1....1..gws-wiz-img.......35i39j0i67.aNBNiUwaKlQ#imgrc=lt_3xL9IZ7O44M:",
+	photo : "https://www.withsneaker.com/wp-content/uploads/2018/06/2018-Off-White-x-Nike-Air-Presto-Black-White-Cone-2.jpg ",
 	scores : [5,5,2,3,1,5,2,1,3,5]
 }, {
 	name : "PF Fyler",
-	photo : "https://www.google.com/search?biw=1103&bih=682&tbm=isch&sa=1&ei=YytWXI-7ApOBk-4P1MSBwA0&q=pf+fyler&oq=pf+fyler&gs_l=img.3..0i10i24.3084.4289..4552...0.0..0.226.314.1j0j1......1....1..gws-wiz-img.OfJCkIJRrNc#imgrc=I86OUKGeqqmnqM:",
+	photo : "https://m.media-amazon.com/images/I/81YGXk7AyFL.jpg",
 	scores : [1,5,1,1,1,1,1,1,5,5]
 }, {
 	name : "ADIDAS Ultra boost",
-	photo : "https://www.google.com/search?q=adidas+ultra+boost&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjCl4KKnp7gAhWL7Z8KHRVqAZEQ_AUIDygC&biw=1103&bih=682#imgrc=xpuOwHgcv5yO2M:",
+	photo : "https://ssld.ulximg.com/image/full/cover/1547669646_91de1a07c867f6a73c83bb985aacdf2d.jpg/29899f76cbfd9a5cbdfcf1d652bb5eb7/1547669646_24a0822ab77e834227688994b44586d8.jpg",
 	scores : [5,3,2,3,2,5,1,3,3,4]
 }];
 
